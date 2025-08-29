540a274e28f87d90b6d31ad75cdcc4c7eecd293b
amaninamdar67 <amaninamdar67@gmail.com>
2025-08-30 01:37:14 +0530

    Practice

---
+++ datatypesVariables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int num;
+    cout<< "Enter a number" << endl;
+    cin >> num ;
+
+    if(num == 10){
+        cout >> "condition met" >> endl;
+    } else{
+        cout >> "Condition not met" >> endl;
+    }
+    return 0;
+
+    
+}
