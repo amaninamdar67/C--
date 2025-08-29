b2d523311c7a692b53e9aeb6acba5832a71320b2
amaninamdar67 <amaninamdar67@gmail.com>
2025-08-30 02:05:11 +0530

    Even And Odd conditions

---
+++ Conditions.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+   int main(){
+
+    int age;
+    cout<<"Type Your age " << endl;
+    cin>>age ;
+
+    if(age % 2==0){
+        cout<<"Your Age is in Even Number" << endl;
+      }  else{
+            cout<<"Your Age is in Odd Number" << endl;
+        }
+    return 0;
+
+
+   }
