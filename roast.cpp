f0633ce9a5eb5985f063ac0f1f2c3890e59061b5
amaninamdar67 <amaninamdar67@gmail.com>
2025-09-06 23:03:23 +0530

    well todays experience

---
+++ roast.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{ 
+string sasta ="BGMIT";
+string canjush;
+string college; 
+
+cout << "sasta college with " ;
+cin>> canjush;
+
+cin>> college ; 
+cout<< "we pay 1 lakh every year to " << sasta << " they still cant buy a proper micrpphone" << endl;
+
+ return 0;
+}
