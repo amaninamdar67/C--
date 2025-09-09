43cd687bafcd7aeeed13aedb2edf940e2d7bb1a8
amaninamdar67 <amaninamdar67@gmail.com>
2025-09-09 23:59:05 +0530

    Revision

---
+++ practice.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age = 21;
+    float height = 5.7;
+    char grade = 'A';
+    string name = "Aman";
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student? " << isStudent << endl;
+
+    return 0;
+}
